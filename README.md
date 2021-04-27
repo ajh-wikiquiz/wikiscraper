304d5e20be1abb405f1d5016f55871b767ec90d6
Allen <allenjhuang92@gmail.com>
2021-04-27 16:58:31 -0500

    Look for some elements programmatically and reorganized code.

--- README.md
+++ README.md
@@ -1,19 +1,36 @@
 # wikiscraper
+Parses and returns text of the given Wikipedia article broken up into sections.
 
-### Example input
+This is intended to be deployed as a web service. A live example can be found at https://ajh-wikiscraper.herokuapp.com/.
+The wikiscraper accepts HTTP GET and POST requests. The POST requests must be in JSON format.
+
+
+### Example inputs
 ```
-{
-    "url": "https://en.wikipedia.org/wiki/Pet_door"
-}
-```
-OR
-```
-{
-    "article": "Pet door"
-}
+{"url": "https://en.wikipedia.org/wiki/Pet_door"}
 ```
 
-### Example output
+```
+{"article": "Pet door"}
+```
+
+article is ignored if url is set. In other words, url is prioritized over article.
+```
+{"article": "gibberish", "url": "https://en.wikipedia.org/wiki/Pet_door"}
+```
+
+As long as /wiki/ is in the URL, the scraper can attempt to parse the article.
+```
+{"url": "https://www.mediawiki.org/wiki/Hackathons"}
+```
+
+If the parsed text isn't to your liking, setting raw to true will return the unparsed text.
+```
+{"url": "https://www.mediawiki.org/wiki/Hackathons", "raw": true}
+```
+
+
+### Example outputs
 ```
 {
     "data": {
@@ -27,26 +44,47 @@ OR
 }
 ```
 
-## Installation
+With raw set to true
 ```
-shard install
+{
+    "data": {
+        "article": "Pet door",
+        "contents": "<div class=\"mw-parser-output\">..."
+    }
+}
 ```
 
-## Usage
+When both the article name and url are missing, or when the url doesn't contain /wiki/
 ```
-crystal build src/wikiscraper.cr --release
+{
+    "error":
+    {
+      "message": "An article name or valid Wikipedia URL must be passed."
+    }
+}
 ```
+
+When the article doesn't exist
+```
+{
+    "data": {
+        "article": "Pet_dooro",
+        "contents": "The page you specified doesn't exist."
+    }
+}
+```
+
 
 ## Development
 ```
+shard install
 crystal run src/wikiscraper.cr
 ```
 
-## Contributing
 
+## Contributing
 1. Fork it (<https://github.com/your-github-user/wikiscraper/fork>)
 2. Create your feature branch (`git checkout -b my-new-feature`)
 3. Commit your changes (`git commit -am 'Add some feature'`)
 4. Push to the branch (`git push origin my-new-feature`)
 5. Create a new Pull Request
-
--- src/wikiscraper.cr
+++ src/wikiscraper.cr
@@ -4,6 +4,9 @@ require "myhtml"
 
 require "json"
 
+# Used when just the article name is passed in.
+ENGLISH_WIKIPEDIA_API_ENDPOINT = "https://en.wikipedia.org/w/api.php"
+
 
 # CORS
 
@@ -31,17 +34,32 @@ end
 get "/" do |env|
   # Check parameters.
   if env.params.query.size != 0
-    if env.params.query.has_key?("article") && !env.params.query["article"].nil?
-      article = env.params.query["article"].as(String)
-    elsif env.params.query.has_key?("url") && !env.params.query["url"].nil?
+    if (
+      env.params.query.has_key?("url") &&
+      !env.params.query["url"].nil? &&
+      env.params.query["url"] != ""
+    )
+      p! env.params.query["url"]
       url = env.params.query["url"].as(String)
-      article = get_article_name(url)
+    elsif env.params.query.has_key?("article") && !env.params.query["article"].nil?
+      article = env.params.query["article"].as(String)
+    end
+
+    if (
+      env.params.query.has_key?("raw") &&
+      !env.params.query["raw"].nil? &&
+      env.params.query["raw"].as(String).downcase == "true"
+    )
+      is_raw = true
     end
   end
 
   # Get article contents.
-  if article
-    article_contents = get_article_contents(article)
+  if url
+    article_contents = get_article_contents(url: url, raw?: is_raw)
+    article = get_article_name_from(url: url)
+  elsif article
+    article_contents = get_article_contents(article: article, raw?: is_raw)
   else
     next {"error": {
       "message": "A valid Wikipedia url or article name must be passed.",
@@ -61,27 +79,40 @@ end
 post "/" do |env|
   # Check parameters.
   if env.params.json.size != 0
-    if env.params.json.has_key?("article") && !env.params.json["article"].nil?
-      article = env.params.json["article"].as(String)
-    elsif env.params.json.has_key?("url") && !env.params.json["url"].nil?
-      url = env.params.json["url"].as(String)
-      article = get_article_name(url)
-    end
+    params = env.params.json
   elsif env.params.body.size != 0
-    if env.params.body.has_key?("article") && !env.params.body["article"].nil?
-      article = env.params.body["article"].as(String)
-    elsif env.params.body.has_key?("url") && !env.params.body["url"].nil?
-      url = env.params.body["url"].as(String)
-      article = get_article_name(url)
+    params = env.params.body
+  end
+
+  if params
+    if (
+      params.has_key?("url") &&
+      !params["url"].nil? &&
+      params["url"] != ""
+    )
+      url = params["url"].as(String)
+    elsif params.has_key?("article") && !params["article"].nil?
+      article = params["article"].as(String)
+    end
+
+    if (
+      params.has_key?("raw") &&
+      !params["raw"].nil? &&
+      params["raw"].as(String).downcase == "true"
+    )
+      is_raw = true
     end
   end
 
   # Get article contents.
-  if article
-    article_contents = get_article_contents(article)
+  if url
+    article_contents = get_article_contents(url: url, raw?: is_raw)
+    article = get_article_name_from(url)
+  elsif article
+    article_contents = get_article_contents(article: article, raw?: is_raw)
   else
     next {"error": {
-      "message": "A valid Wikipedia url or article name must be passed.",
+      "message": "An article name or valid Wikipedia URL must be passed.",
     },}.to_json
   end
 
@@ -99,8 +130,7 @@ end
 # Helper functions
 
 # Returns the article name as a String from the url.
-# TODO: Refactor.
-def get_article_name(url : String)
+def get_article_name_from(url : String)
   wiki = url.index("/wiki/")
   id = url.rindex("#")
   if wiki
@@ -114,6 +144,7 @@ end
 def get_article_contents(
   article : String | Nil = nil,
   url : String | Nil = nil,
+  raw? : Bool | Nil = false,
 )
   # Either article or url must be passed into this function.
   if !article && !url
@@ -121,9 +152,29 @@ def get_article_contents(
   end
 
   # Get Wikipedia contents.
-  if article
-    parser = Myhtml::Parser.new(JSON.parse(Crest.get(
-      "https://en.wikipedia.org/w/api.php",
+  if url
+    article = get_article_name_from(url)
+    if !article
+      return nil
+    end
+
+    begin
+      wikitext = Crest.get(url).body
+    rescue ex : Crest::NotFound
+      return "The page you specified doesn't exist."
+    rescue ex : Crest::InternalServerError
+      return "Wikipedia Internal Server Error"
+    end
+
+    if raw?
+      return wikitext
+    end
+
+    parser = Myhtml::Parser.new(wikitext)
+
+  elsif article
+    response = JSON.parse(Crest.get(
+      ENGLISH_WIKIPEDIA_API_ENDPOINT,
       params: {
         :action => "parse",
         :page => article,
@@ -131,44 +182,47 @@ def get_article_contents(
         :formatversion => 2,
         :format => "json",
       }
-    ).body)["parse"]["text"].as_s)
-  elsif url
-    article = get_article_name(url)
-    parser = Myhtml::Parser.new(
-      Crest.get(url).body)
+    ).body)
+    if response["parse"]? && response["parse"]["text"]?
+      wikitext = response["parse"]["text"].as_s
+    elsif response["error"]? && response["error"]["info"]?
+      return response["error"]["info"].as_s
+    else
+      return nil
+    end
+
+    if raw?
+      return wikitext
+    end
+
+    parser = Myhtml::Parser.new(wikitext)
+
+  else
+    return nil
   end
 
-  # TODO: Check for nil on parser here.
-
-  # Add summary text.
-  table_of_contents = [{".mw-parser-output", "0", article}]
+  # Add summary section.
+  table_of_contents = [
+    {"id": ".mw-parser-output", "tocnumber": "0", "toctext": article.not_nil!}
+  ]
   # Read table of contents.
   parser.not_nil!
     .css(%q{div[id="toc"] li[class*="toclevel"]})
     .each do |node|
       id = node.child!.attribute_by("href").not_nil!
       node = node.child!
-      tocnumber, toctext = node.inner_text.split(2)  # TODO: Don't hardcode.
-      table_of_contents.push({id, tocnumber, toctext})
+      tocnumber, toctext = node.inner_text.split(2)  # Need to split on 2 here.
+      table_of_contents.push(
+        {"id": id, "tocnumber": tocnumber, "toctext": toctext})
     end
 
-  # [{"number": 1, "title": "Purpose", "content": "A pet door..."}]
+  # Example: [{"number": 1, "title": "Purpose", "content": "A pet door..."},]
   sections = Array(
     NamedTuple(number: String, title: String, content: String)).new
 
+  # Get sections of content.
   table_of_contents.each do |toc|
-    toctext_downcase = toc[2].downcase
-    if (
-      toctext_downcase == "see also" ||
-      toctext_downcase == "notes" ||
-      toctext_downcase == "references" ||
-      toctext_downcase == "further reading" ||
-      toctext_downcase == "popular reading" ||
-      toctext_downcase == "university textbooks and monographs" ||
-      toctext_downcase == "review papers" ||
-      toctext_downcase == "external links" ||
-      toctext_downcase == "videos"
-    )
+    if skip_section?(toc["toctext"])
       next
     else
       append_section(sections, toc, parser.not_nil!, article)
@@ -179,18 +233,20 @@ def get_article_contents(
   return sections
 end
 
+# Pushes a section of content onto the sections array.
 # Helper function for `get_article_contents`.
-# TODO: Refactor.
+# Jumps to a section noted from the table of contents to begin parsing.
 def append_section(
   sections : Array(NamedTuple(number: String, title: String, content: String)),
-  toc : Tuple(String, String, String),
+  toc : NamedTuple(id: String, tocnumber: String, toctext: String),
   parser : Myhtml::Parser,
   article : String
 )
-  if toc[2] != article
-    css_selector = %(span[id="#{toc[0].sub("#", "")}"])
+  # TODO: Find a better way to special case the summary section.
+  if toc["toctext"] != article
+    css_selector = %(span[id="#{toc["id"].sub("#", "")}"])
   else
-    css_selector = %(div[class="#{toc[0].sub(".", "")}"])
+    css_selector = %(div[class="#{toc["id"].sub(".", "")}"])
   end
 
   # Create content string for each section except for References.
@@ -198,104 +254,212 @@ def append_section(
   parser
     .css(css_selector)
     .each do |node|
-      if toc[2] != article
+      if toc["toctext"] != article
         node = node.parent!.next!
       else
         node = node.child!
       end
 
       while node
-        # Stop appending for section once the next section has been reached.
-        if (
-          (
-            (node.tag_name == "h2" || node.tag_name == "h3") &&
-            node.child &&
-            node.child!.attribute_by("class") &&
-            node.child!.attribute_by("class").not_nil!.includes?("mw-headline")
-          ) ||
-          node.attribute_by("class") == "reflist" ||
-          (
-            node.tag_name == "div" &&
-            (
-              (
-                node.attribute_by("id") &&
-                node.attribute_by("id").not_nil! == ("toc")
-              ) ||
-              (
-                node.attribute_by("class") &&
-                (
-                  node.attribute_by("class").not_nil!.includes?("toclimit-4")
-                )
-              )
-            )
-          )
-        )
+        # Stop appending text once the start of another section is found.
+        if breakpoint?(node)
           break
         end
-        # Ignore captions, sidebars, and random styles.
-        if (
-          (
-            node.tag_name == "div" &&
-            node.attribute_by("class") &&
-            (
-              node.attribute_by("class").not_nil!.includes?("thumb") ||
-              node.attribute_by("class").not_nil!.includes?("hatnote") ||
-              node.attribute_by("class").not_nil!.includes?("shortdescription") ||
-              node.attribute_by("class").not_nil!.includes?("mod-gallery")
-            )
-          ) ||
-          (
-            node.tag_name == "table"
-          ) ||
-          (
-            node.tag_name == "style"
-          )
-        )
-        elsif (
-          node.tag_name == "span" &&
-          node.attribute_by("class") &&
-          node.attribute_by("class").not_nil!.includes?("mwe-math-element") &&
-          node.child &&
-          node.child!.child &&
-          node.child!.child!.attribute_by("alttext")
-        )
-          # Math element
-          content += node.child!.child!.attribute_by("alttext").not_nil!
-        elsif (
-          node.tag_name == "dl" &&
-          node.child &&
-          node.child!.tag_name == "dd" &&
-          node.child!.child &&
-          node.child!.child!.tag_name == "span" &&
-          node.child!.child!.child &&
-          node.child!.child!.child!.tag_name == "span" &&
-          node.child!.child!.child!.child &&
-          node.child!.child!.child!.child!.tag_name == "math" &&
-          node.child!.child!.child!.child!.attribute_by("alttext")
-        )
-          # Math element
-          content += node.child!.child!.child!.child!.attribute_by("alttext").not_nil!
-        elsif (
-          node.tag_name == "span" &&
-          node.attribute_by("class") &&
-          node.attribute_by("class").not_nil!.includes?("mwe-math-element") &&
-          node.child &&
-          node.child!.child &&
-          node.child!.child!.attribute_by("alttext")
-        )
-          # Math element
-          content += node.child!.child!.attribute_by("alttext").not_nil!
+        # Ignore captions, sidebars, and random style tags.
+        if ignore?(node)
+          # Don't append to content if it's a node to be ignored.
         else
-          # Strip all references and append.
-          content += node.inner_text.gsub(/\[[0-9]+\]|\[[a-z]+\]|\[edit\]|\[Note [0-9]+\]/, "")
+          if math_element = math_element?(node)
+            content += " #{math_element} "
+          else
+            content += remove_references(node.inner_text)
+          end
         end
+
         node = node.next
       end
+
     end
-  sections.push({"number": toc[1], "title": toc[2], "content": content})
+
+  sections.push(
+    {
+      "number": toc["tocnumber"],
+      "title": toc["toctext"],
+      "content": replace_whitespaces(content).strip()
+    }
+  )
 end
 
-# TODO: Create recursive function to get latex from alttext.
+# Returns true if the string matches a section to be skipped, else false.
+def skip_section?(toctext : String)
+  toctext_downcase = toctext.downcase
+  if (
+    toctext_downcase == "see also" ||
+    toctext_downcase == "notes" ||
+    toctext_downcase == "references" ||
+    toctext_downcase == "further reading" ||
+    toctext_downcase == "popular reading" ||
+    toctext_downcase == "university textbooks and monographs" ||
+    toctext_downcase == "review papers" ||
+    toctext_downcase == "external links" ||
+    toctext_downcase == "videos"
+  )
+    return true
+  end
+  return false
+end
+
+# Returns true if it looks like the current section has ended or the next
+# section has been reached.
+def breakpoint?(node : Myhtml::Node)
+  current = node
+  while current
+    if (
+      (
+        current.attribute_by("class") &&
+        current.attribute_by("class").not_nil!.includes?("mw-headline")
+      ) ||
+      current.attribute_by("class") == "reflist" ||
+      (
+        current.tag_name == "div" &&
+        (
+          (
+            current.attribute_by("id") &&
+            current.attribute_by("id").not_nil! == ("toc")
+          ) ||
+          (
+            current.attribute_by("class") &&
+            (
+              current.attribute_by("class").not_nil!.includes?("toclimit-4")
+            )
+          )
+        )
+      )
+    )
+      # Next section or end of section has been reached.
+      return true
+    end
+
+    if (
+      /h[0-9]/i.match(current.tag_name) &&
+      current.child
+    )
+      # Search children recursively.
+      if (is_breakpoint = breakpoint?(current.child!))
+        return is_breakpoint
+      end
+    end
+
+    # From what I've seen, we only need to check siblings if it's a span tag.
+    if current.next && current.next!.tag_name == "span"
+      current = current.next
+    else
+      break
+    end
+  end
+
+  return false
+end
+
+# Returns true if the node should be ignored for text appending.
+def ignore?(node : Myhtml::Node)
+  if (
+    (
+      node.tag_name == "div" &&
+      node.attribute_by("class") &&
+      (
+        node.attribute_by("class").not_nil!.includes?("thumb") ||
+        node.attribute_by("class").not_nil!.includes?("hatnote") ||
+        node.attribute_by("class").not_nil!.includes?("shortdescription") ||
+        node.attribute_by("class").not_nil!.includes?("mod-gallery")
+      )
+    ) ||
+    (
+      node.tag_name == "table"
+    ) ||
+    (
+      node.tag_name == "style"
+    )
+  )
+    return true
+  end
+  return false
+end
+
+# Returns the math element if found in the node, else nil.
+def math_element?(node : Myhtml::Node | Nil)
+  current = node
+  while current
+    # Confirmed math element
+    if current.tag_name == "math"
+      if current.attribute_by("alt")
+        return current.attribute_by("alt")
+      end
+      if current.attribute_by("alttext")
+        return current.attribute_by("alttext")
+      end
+    elsif (
+      current.tag_name == "annotation" &&
+      current.attribute_by("encoding") &&
+      current.attribute_by("encoding").not_nil!.includes?("application/x-tex")
+    )
+      return current.inner_text
+    elsif (
+      current.tag_name == "img" &&
+      current.attribute_by("class") &&
+      current.attribute_by("class").not_nil!.includes?(
+        "mwe-math-fallback-image-inline")
+    )
+      if current.attribute_by("alt")
+        return current.attribute_by("alt")
+      end
+      if current.attribute_by("alttext")
+        return current.attribute_by("alttext")
+      end
+      # Else do nothing.
+
+    # If it looks like it could be a math element...
+    elsif (
+      current.tag_name == "span" ||
+      current.tag_name == "dl" ||
+      current.tag_name == "dd" ||
+      current.tag_name == "semantics" ||
+      current.tag_name == "dd" ||
+      (
+        current.attribute_by("class") &&
+        current.attribute_by("class").not_nil!.includes?("mwe-math-element")
+      )
+    )
+      # Search deeper recursively.
+      if (child = math_element?(current.child))
+        return child
+      end
+
+    else  # Not likely to be a math element.
+      break
+    end
+
+    # If the next element has the same tag name, it's likely to be a different
+    # math element if it is one.
+    if current.next && current.tag_name == current.next!.tag_name
+      break
+    end
+
+    current = current.next
+  end
+
+  return nil
+end
+
+# Removes all the wikipedia source references within text.
+def remove_references(inner_text : String)
+  return inner_text.gsub(/\[[0-9]+\]|\[[a-z]+\]|\[edit\]|\[Note [0-9]+\]/, "")
+end
+
+# Replaces some of the whitespace with a single space.
+def replace_whitespaces(inner_text : String)
+  return inner_text.gsub(/\t|\s\s+/, " ")
+end
 
 
 # Main
